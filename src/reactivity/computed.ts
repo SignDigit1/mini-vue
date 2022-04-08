9f2123823829d837c0c8a81da76d46b7ee5d0dfc
fujun <fujunchn@qq.com>
2022-04-08 15:24:15 +0800

    :sparkles: feat(reactivity): computed添加缓存

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -1,24 +1,38 @@
-import { ReactiveEffect } from './effect';
-import { Ref } from './ref';
-
 /*
  * @Author: jun.fu<fujunchn@qq.com>
  * @LastEditors: jun.fu<fujunchn@qq.com>
  * @Description: file content
  * @Date: 2022-04-08 09:44:26
- * @LastEditTime: 2022-04-08 09:57:54
+ * @LastEditTime: 2022-04-08 15:22:04
  * @FilePath: /mini-vue3/src/reactivity/computed.ts
  */
+import { ReactiveEffect } from './effect';
+import { Ref } from './ref';
+
 class ComputedImpl implements Ref {
   private _effect: ReactiveEffect;
+  // 用于保存 getter 函数的执行结果
+  private _value;
+  // 用于记录是否不使用缓存
+  private _dirty = true;
 
   constructor(getter) {
     // 利用 getter 函数创建 ReactiveEffect 类的实例
-    this._effect = new ReactiveEffect(getter);
+    this._effect = new ReactiveEffect(
+      getter,
+      // 用于关闭缓存
+      () => {
+        this._dirty = true;
+      }
+    );
   }
   // value property 的 get 返回调用私有 property _effect 的 run 方法的返回值，即调用 getter 函数的返回值
   get value() {
-    return this._effect.run();
+    if (this._dirty) {
+      this._value = this._effect.run();
+      this._dirty = false;
+    }
+    return this._value;
   }
 }
 
--- src/reactivity/test/computed.spec.ts
+++ src/reactivity/test/computed.spec.ts
@@ -3,7 +3,7 @@
  * @LastEditors: jun.fu<fujunchn@qq.com>
  * @Description: file content
  * @Date: 2022-04-08 09:54:36
- * @LastEditTime: 2022-04-08 09:55:40
+ * @LastEditTime: 2022-04-08 15:23:29
  * @FilePath: /mini-vue3/src/reactivity/test/computed.spec.ts
  */
 import { reactive } from '../reactive';
@@ -16,5 +16,28 @@ describe('reactivity/computed', () => {
     expect(cValue.value).toBe(1);
     value.foo = 2;
     expect(cValue.value).toBe(2);
+  });
+
+  it('should compute lazily', () => {
+    const value = reactive({ foo: 1 });
+    const getter = jest.fn(() => value.foo);
+    const cValue = computed(getter);
+
+    // 在获取 ref 对象的 value property 的值时才执行 getter
+    expect(getter).not.toHaveBeenCalled();
+    expect(cValue.value).toBe(1);
+    expect(getter).toHaveBeenCalledTimes(1);
+    // 若依赖的响应式对象的 property 的值没有更新，则再次获取 ref 对象的 value property 的值不会重复执行 getter
+    cValue.value;
+    expect(getter).toHaveBeenCalledTimes(1);
+    // 修改依赖的响应式对象的 property 的值时不会执行 getter
+    value.foo = 1;
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // 在依赖的响应式对象的 property 的值没有更新后，获取 ref 对象的 value property 的值再次执行 getter
+    expect(cValue.value).toBe(1);
+    expect(getter).toHaveBeenCalledTimes(2);
+    cValue.value;
+    expect(getter).toHaveBeenCalledTimes(2);
   });
 });

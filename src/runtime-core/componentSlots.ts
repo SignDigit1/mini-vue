b60f774aab5b2aa04b24848a7364a936232ff0b1
fujun <fujunchn@qq.com>
2022-04-18 14:54:24 +0800

    fix(runtime-core): 完成具名slot

--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -3,20 +3,26 @@
  * @LastEditors: jun.fu<fujunchn@qq.com>
  * @Description: file content
  * @Date: 2022-04-13 23:15:34
- * @LastEditTime: 2022-04-14 00:31:23
- * @FilePath: \mini-vue3\src\runtime-core\componentSlots.ts
+ * @LastEditTime: 2022-04-18 14:53:11
+ * @FilePath: /mini-vue3/src/runtime-core/componentSlots.ts
  */
 // 用于将 children 赋值给组件实例对象的 slots property
 function initSlots(instance, children) {
-  const slots = {};
-  // 遍历 children，将其 property 对应的 VNode 数组挂载到 slots 对象上
-  for (const key in children) {
-    const value = children[key];
+  // 具名导入
+  if (typeof children === 'object' && !children.type && !children.shapeFlag) {
+    const slots = {};
+    // 遍历 children，将其 property 对应的 VNode 数组挂载到 slots 对象上
+    for (const key in children) {
+      const value = children[key];
 
-    slots[key] = Array.isArray(value) ? value : [value];
+      slots[key] = value;
+    }
+
+    instance.slots = slots;
+  } else {
+    // 非具名导入
+    instance.slots = Array.isArray(children) ? children : [children];
   }
-
-  instance.slots = slots;
 }
 
 export { initSlots };

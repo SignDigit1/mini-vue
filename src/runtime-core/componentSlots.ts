00573953a6ed87d0c4667fdf2aa3aa343ee70631
fujun <fujunchn@qq.com>
2022-04-18 15:27:15 +0800

    Revert ":bug: fix(runtime-core): 自己理解的具名slot实现方式"
    This reverts commit 9a2fe3f155b38dc1d14e5265bd08e727fb36fe83.

--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -3,13 +3,26 @@
  * @LastEditors: jun.fu<fujunchn@qq.com>
  * @Description: file content
  * @Date: 2022-04-13 23:15:34
- * @LastEditTime: 2022-04-18 14:59:48
+ * @LastEditTime: 2022-04-18 14:53:11
  * @FilePath: /mini-vue3/src/runtime-core/componentSlots.ts
  */
 // 用于将 children 赋值给组件实例对象的 slots property
 function initSlots(instance, children) {
   // 具名导入
-  instance.slots = Array.isArray(children) ? children : [children];
+  if (typeof children === 'object' && !children.type && !children.shapeFlag) {
+    const slots = {};
+    // 遍历 children，将其 property 对应的 VNode 数组挂载到 slots 对象上
+    for (const key in children) {
+      const value = children[key];
+
+      slots[key] = value;
+    }
+
+    instance.slots = slots;
+  } else {
+    // 非具名导入
+    instance.slots = Array.isArray(children) ? children : [children];
+  }
 }
 
 export { initSlots };
--- src/shared/renderSlots.ts
+++ src/shared/renderSlots.ts
@@ -3,16 +3,13 @@
  * @LastEditors: jun.fu<fujunchn@qq.com>
  * @Description: file content
  * @Date: 2022-04-14 00:11:32
- * @LastEditTime: 2022-04-18 15:02:52
- * @FilePath: /mini-vue3/src/shared/renderSlots.ts
+ * @LastEditTime: 2022-04-14 00:11:33
+ * @FilePath: \mini-vue3\src\shared\renderSlots.ts
  */
 function renderSlots(slots, name) {
   // 通过 name 获取相应的插槽
-  // const slot = slots[name];
-  // return slot;
-  return slots.find(vnode => {
-    return vnode?.props?.slotName === name;
-  });
+  const slot = slots[name];
+  return slot;
 }
 
 export { renderSlots };
